50385f2b59b294e05b4b8e20104bbee162ba07e2
ana-sanchez <snchz.rojo@gmail.com>
2020-02-11 21:55:37 +0100

    add style component app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Fragment } from "react";
 import Form from './Form'
 import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-        <Form />
+    <Fragment>
+      <h1>Añade una lista de tarea</h1>
+      <div className="container">
+        <div className='row'>
+          <div className='one-half column'>
+            <Form />
+
+
+          </div>
+
+        </div>
         
     </div>
+    </Fragment>
+   
   );
 }
 
